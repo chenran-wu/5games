3dec3ea44a45182b843744804f32c1fe69df8154
sub15 <chenran1129@gmail.com>
2021-03-06 11:32:06 +0200

    move up paddle_a

--- pong.py
+++ pong.py
@@ -29,5 +29,13 @@ ball.color("white")
 ball.penup()
 ball.goto(0, 0)
 
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+
 while True:
     wn.update()
