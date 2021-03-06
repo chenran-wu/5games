8945c21d1fc1487ba06952b16bbb877ec88482fa
sub15 <chenran1129@gmail.com>
2021-03-06 11:38:18 +0200

    move paddle_b

--- pong.py
+++ pong.py
@@ -39,9 +39,21 @@ def paddle_a_down():
     y -= 20
     paddle_a.sety(y)
 
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
 wn.listen()
 wn.onkeypress(paddle_a_up, "w")
 wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
 
 while True:
     wn.update()
